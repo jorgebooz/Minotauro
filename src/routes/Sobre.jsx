766097ade8ebc1f7b2035d7303ecda3cb07ced1e
Jorge Booz <107008455+jorgebooz@users.noreply.github.com>
2024-04-01 21:57:16 -0300

    feat: start sobre

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,14 +1,29 @@
 import {} from 'react';
-import '../css/estilo.css';
+import '../css/estilo-sobre.css'
+import foguete from '../assets/FOGUETE.svg'
 
 function Sobre() {
   return (
     <>
       <section className="sobre">
-        <h1>Sobre</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam numquam expedita cumque iure, praesentium recusandae, dolorem voluptatem amet dolore laborum voluptatum minima dolorum vitae corrupti non quasi dolores unde illum! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis iste atque maxime velit illum magni quasi? Consequuntur expedita reprehenderit minus, officia culpa quis, voluptas quisquam eligendi in dolore distinctio sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi doloremque minus eligendi voluptate ad ipsam quod, incidunt animi ullam dolor deserunt at a hic dolorem asperiores aperiam, eius repellat molestias;
-        </p>
+        <div className='header'>
+          <div className='titulo'>
+            <h1><a className='azul'>Quem</a> somos nós?</h1>
+          </div>
+          
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+
+        <div className='card'>
+          <img src={foguete} alt="foguetada" />
+          <h1>Lorem Ipsum is simply</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        </div>
+        
       </section>
+
+
     </>
   );
 }
