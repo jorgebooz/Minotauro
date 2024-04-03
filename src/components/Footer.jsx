c45e9494e753b0197c7239fa59fecf712460dc5b
Jorge Booz <107008455+jorgebooz@users.noreply.github.com>
2024-04-02 21:20:24 -0300

    fix: footer colaborators names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,23 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <div className="endereço">
-        <h5>Endereço:</h5>
-        <p>BR, São Paulo</p>
-        <h5>Contato</h5>
-        <p>11 97070-7070</p>
-        <p>fiap@fiap.com.br</p>
+      <div className='top'>
+        <div className="endereço">
+          <h5>Endereço:</h5>
+          <p>BR, São Paulo</p>
+          <h5>Contato</h5>
+          <p>11 97070-7070</p>
+          <p>fiap@fiap.com.br</p>
+        </div>
+        <div className='integrantes'>
+            <p>Caio Hideki</p>
+            <p>Diana Leticia</p>
+            <p>Erick Molina</p>
+            <p>João Carvalho</p>
+            <p>Jorge Booz</p>
+        </div>
       </div>
-      <div className='integrantes'>
-          <p>Caio Hideki</p>
-          <p>Diana Leticia</p>
-          <p>Erick Molina</p>
-          <p>João Carvalho</p>
-          <p>Jorge Booz</p>
-      </div>
-      <div></div>
+      
       <div className="copy-right">
         <p>© Todos os direitos reservados a FIAP</p>
       </div>
