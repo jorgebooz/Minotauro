c92620470d470b9cd996ddf10500f5ddc954219a
Jorge Booz <107008455+jorgebooz@users.noreply.github.com>
2024-05-20 12:45:44 -0300

    feat: logout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,60 @@
-import {} from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import '../css/estilo.css';
-import logo from "../assets/MINOTAURO.svg"
+import logo from "../assets/MINOTAURO.svg";
 
 function Nav() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const usuario = sessionStorage.getItem('usuario');
+    const senha = sessionStorage.getItem('senha');
+    if (usuario && senha) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    sessionStorage.clear();
+    setIsLoggedIn(false);
+    navigate('/');
+  };
+
   return (
     <>
       <header className="menu">
-          <nav className="nav-menu">
-              <Link to="/" className="tlink">
-                <img src={logo} alt="" />
-              </Link>
-            <ul>
+        <nav className="nav-menu">
+          <Link to="/" className="tlink">
+            <img src={logo} alt="" />
+          </Link>
+          <ul>
             <Link to="/" className="tlink">
-                Home
-              </Link>{' '}
-              <Link to="/produtos" className="tlink">
-                Produtos
-              </Link>{' '}
-              {''}
-              <Link to="/contato" className="tlink">
-                Contato
+              Home
+            </Link>
+            <Link to="/produtos" className="tlink">
+              Produtos
+            </Link>
+            <Link to="/contato" className="tlink">
+              Contato
+            </Link>
+            <Link to="/sobre" className="tlink">
+              Sobre
+            </Link>
+            {isLoggedIn ? (
+              <Link onClick={handleLogout} className="tlink">
+                Logout
               </Link>
-              <Link to="/sobre" className="tlink">
-                Sobre
-              </Link>
+            ) : (
               <Link to="/login" className="tlink">
                 Login
               </Link>
-              
-            </ul>
+            )}
+          </ul>
         </nav>
       </header>
     </>
   );
 }
+
 export default Nav;
