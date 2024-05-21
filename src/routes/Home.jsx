f2102db943b32be34c1db388bb093f594c903e29
Jorge Booz <107008455+jorgebooz@users.noreply.github.com>
2024-05-20 21:09:29 -0300

    redesign: Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,25 +1,80 @@
-import {} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/estilo.css';
-import jordan from "../assets/JORDAN.svg"
-import colecoes from '../assets/COLECOES.svg'
+import jordan from "../assets/JORDAN.svg";
+import mizuno from '../assets/MIZUNO.svg';
+import forum from '../assets/FORUM.svg';
+import { motion } from "framer-motion";
+import vermelho from '../assets/TENIS-VERMELHO.svg'
+import branco from '../assets/TENIS-BRANCO.svg'
+import azul from '../assets/TENIS-AZUL.svg'
+import verde from '../assets/TESNIS-VERDE.svg'
+import cinco from '../assets/FIVE-STARS.svg'
 
 function Home() {
-  return (
-    <>
-      <div className="home-main">
+  const images = [
+    jordan,
+    mizuno,
+    forum
+  ];
 
-        <div className="jordan">
-          <img src={jordan} alt="Jordan" className='home-image'/>
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-          <a href="/produtos" className="btn">
-            MAIS PRODUTOS &#8594;
-          </a>       
-        </div>
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(timer);
+  }, [images.length]);
 
-        <img src={colecoes} alt="colecoes" className='home-image'/>
+  return (  
+      <div className="mainHome">
+        
+        <p className='textHome'>
+          <h1 className='tituloHome'>Bem-vindo a MINOTAURO!</h1>
 
+          Bem-vindo à Minotauro, sua loja de artigos esportivos! Aqui, você encontra produtos de alta qualidade para todas as suas necessidades esportivas, desde roupas e calçados até 
+          equipamentos. Explore nosso site e descubra como podemos ajudá-lo a alcançar seus objetivos e aprimorar seu desempenho. Conte conosco para fazer parte da sua jornada esportiva!
+        </p>
+        <a href="/produtos">
+          <motion.img 
+            src={images[currentIndex]} 
+            alt='carousel'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }} 
+            transition={{ duration: 1 }} 
+            className='homeImage'
+          />
+        </a>
+
+          <div className="produtos-grade produtosHome">
+              <Link to="/produtos" className='produtos-item'> {/* Use Link aqui também */}
+              <img src={vermelho} alt="vermelho" />
+              <p className='produtos-titulo'>HYPERBOOST</p>
+              <p className="produtos-preco">160$</p>
+              <img src={cinco} alt="estrelas" />
+            </Link>
+            <Link to="/produtos" className='produtos-item'> {/* Use Link aqui também */}
+              <img src={branco} alt="vermelho" />
+              <p className='produtos-titulo'>ULTRAWIDE</p>
+              <p className="produtos-preco">140$</p>
+              <img src={cinco} alt="estrelas" />
+            </Link>
+            <Link to="/produtos" className='produtos-item'> {/* Use Link aqui também */}
+              <img src={azul} alt="vermelho" />
+              <p className='produtos-titulo'>GIGANTAMAX</p>
+              <p className="produtos-preco">180$</p>
+              <img src={cinco} alt="estrelas" />
+            </Link>
+            <Link to="/produtos" className='produtos-item'> {/* Use Link aqui também */}
+              <img src={verde} alt="vermelho" />
+              <p className='produtos-titulo'>MEGA-EVOLUTION</p>
+              <p className="produtos-preco">200$</p>
+              <img src={cinco} alt="estrelas" />
+            </Link>
+          </div>
+            
       </div>
-    </>
   );
 }
 
